813c31498d991b627f9b9c7274bd8e4d934052a6
Charles Boudry <chboudry@outlook.com>
2023-01-12 09:24:54 +0100

    Update deploy_kubeflow.sh

--- Kubeflow-1.6.1-on-AKS-1.24.6/deploy_kubeflow.sh
+++ Kubeflow-1.6.1-on-AKS-1.24.6/deploy_kubeflow.sh
@@ -1,1 +1,2 @@
+az aks get-credentials --resource-group $RESOURCE_GROUP_NAME --name $AKS_NAME --admin
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
