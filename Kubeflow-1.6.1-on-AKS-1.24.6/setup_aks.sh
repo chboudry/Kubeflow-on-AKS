6576b194292493aa806fd04a576fbbd0d8e63d2a
Charles Boudry <chboudry@outlook.com>
2023-01-12 09:25:14 +0100

    Update setup_aks.sh

--- Kubeflow-1.6.1-on-AKS-1.24.6/setup_aks.sh
+++ Kubeflow-1.6.1-on-AKS-1.24.6/setup_aks.sh
@@ -1,2 +1,1 @@
 az aks create -g $RESOURCE_GROUP_NAME -n $AKS_NAME -s $AGENT_SIZE -c $AGENT_COUNT -l $LOCATION --kubernetes-version $AKS_VERSION --generate-ssh-keys
-az aks get-credentials --resource-group $RESOURCE_GROUP_NAME --name $AKS_NAME --admin
